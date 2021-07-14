ac969e068a98140316ef88a741dd43d6a06b8862
xorua117 <xorua117@gmail.com>
2021-07-14 21:25:21 +0900

    Navigating to the filter events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,23 @@
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
 import EventSearch from '../../components/events/EventsSearch';
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler}/>
       <EventList items={events} />
     </Fragment>
   );
