a334b29dbddc443ef318058b2d165d35f6f4fca2
xorua117 <xorua117@gmail.com>
2021-07-17 22:11:46 +0900

    Using context data in components

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,25 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 
 import MainHeader from "./MainHeader";
+import Notification from "../ui/Notification";
+import NotificationContext from "../../store/NotificationContext";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
