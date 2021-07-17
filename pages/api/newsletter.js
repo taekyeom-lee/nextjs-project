1f81644b346295925b4f6c199cf68176445863f1
xorua117 <xorua117@gmail.com>
2021-07-17 21:02:52 +0900

    More error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,18 +1,4 @@
-import { MongoClient } from "mongodb";
-
-import dbConfig from "../../config/db";
-
-async function connectDatabase() {
-  const client = await MongoClient.connect(`${dbConfig.dev}`);
-
-  return client;
-}
-
-async function insertDocument(client, document) {
-  const db = client.db();
-
-  await db.collection("newsletter").insertOne(document);
-}
+import { connectDatabase, insertDocument } from "../../helpers/db-util";
 
 async function handler(req, res) {
   if (req.method === "POST") {
@@ -33,7 +19,7 @@ async function handler(req, res) {
     }
 
     try {
-      await insertDocument(client, { email: userEmail });
+      await insertDocument(client, "newsletter", { email: userEmail });
       client.close();
     } catch (error) {
       res.status(500).json({ message: "Inserting data failed!" });
